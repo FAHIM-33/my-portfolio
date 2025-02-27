991aa5057db66442d84c4a575642f29027a5cd06
FAHIM-33 <shahriyerfahim2012@gmail.com>
2025-02-27 19:51:11 +0600

    update

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -10,8 +10,8 @@ const Banner = () => {
     return (
         <div className='relative mt-16 cont'>
 
-            <div className='ml-4 md:ml-0 flex flex-col md:flex-row gap-10 md:gap-0 md:items-center justify-between px-4 lg:px-0'>
-                <h1 className='text-[70px] md:text-[100px] font-sans font-bold leading-[65px] md:leading-[80px]'>
+            <div className='ml-4 2xl:ml-0 flex flex-col md:flex-row gap-10 md:gap-0 md:items-center justify-between pl-2 md:pr-4 2xl:px-0'>
+                <h1 className='text-[68px] md:text-[75px] lg:text-[100px] font-sans font-bold leading-[65px] md:leading-[80px]'>
                     Web<span className='animate-ping'>.</span>
                     <br />
                     developer
@@ -28,14 +28,14 @@ const Banner = () => {
                 </div>
             </div>
 
-            <p className='mt-20 md:mt-32 font-sans text-sm text-gray-400  ml-6 md:ml-0'>
+            <p className='mt-20 md:mt-32 font-sans text-sm text-gray-400  ml-4 2xl:ml-0'>
                 I&apos;m
                 <a href="https://github.com/FAHIM-33" title="Go to GitHub profile" target="_blank" rel="noopener noreferrer" className="text-blue-400"> SH Fahim</a>
                 <br />
                 I make fully custom website with
-                <span className='bg-[#1e0f2f] border rounded px-1 mx-1 text-white border-[#7c2bff] '>Next JS</span>
+                <span className='bg-[#1e0f2f] border rounded px-1 mx-1 text-white border-[#7c2bff] whitespace-nowrap '>Next JS</span>
                 or
-                <span className='bg-[#0f1b2f] border rounded px-1 mx-1 text-white border-[#2b7fff]'>React JS</span>
+                <span className='bg-[#0f1b2f] border rounded px-1 mx-1 text-white border-[#2b7fff] whitespace-nowrap'>React JS</span>
             </p>
 
         </div>
